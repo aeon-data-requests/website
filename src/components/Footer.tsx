679d52f7dfd7a60cf090396d23fc4e883a8ff0eb
Lei Nelissen <lei@codified.nl>
2020-11-18 17:53:40 +0100

    Add website-carbon button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import Carbonbadge from 'react-carbonbadge';
 import Container, { TwoPanel } from './Container';
 
 const Wrapper = styled.div`
@@ -17,14 +18,17 @@ function Footer() {
             <Container>
                 <TwoPanel>
                     <div>
-                        <p>Aeon is created and maintained by Lei Nelissen</p>
-                        <p>&copy; 2020 Lei Nelissen</p>
+                        <p>
+                            Aeon is created and maintained by Lei Nelissen
+                            <br />&copy; 2020 Lei Nelissen
+                        </p>
                     </div>
                     <div>
                         <strong>Privacy Policy &amp; Terms of Service</strong>
                         <p>We don&apos;t story any of your data. Period. Also, this software is provided as is. No guarantees, no take-backsies.</p>
                     </div>
                 </TwoPanel>
+                <Carbonbadge />
             </Container>
         </Wrapper>
     )
