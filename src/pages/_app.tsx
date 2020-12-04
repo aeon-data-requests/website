db3501e2e4cfde705b21dd2fc003e778ab851faa
Lei Nelissen <lei@codified.nl>
2020-12-04 20:59:21 +0100

    Fix wrongly sized Font Awesome icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,10 @@ import Menu from 'components/Menu';
 import type { AppProps /*, AppContext */ } from 'next/app'
 import Head from 'next/head';
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 import 'styles/global.css';
 import 'fontsource-ibm-plex-sans/latin-400.css';
 import 'fontsource-ibm-plex-sans/latin-600.css';
