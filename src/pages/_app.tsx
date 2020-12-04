46aff33e5e00f03ade9f4c2bb9447afa2c959aab
Lei Nelissen <lei@codified.nl>
2020-12-04 14:13:46 +0100

    Add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,13 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Head>
                 <title>Aeon - Get a grip on your data</title>
                 <meta name="description" content="Aeon is an app that retrieves your personal data from organizations and the internet, using the power of the GDPR"/>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0000ff" />
+                <meta name="msapplication-TileColor" content="#2d89ef" />
+                <meta name="theme-color" content="#ffffff" />
             </Head>
             <Menu />
             <Component {...pageProps} />
